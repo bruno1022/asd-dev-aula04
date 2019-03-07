f166debc281ab8cf03d181b8b7e5bf6fc45fc809
Bruno Ferreira De Almeida <524787@sga.pucminas.br>
2019-03-07 20:19:43 -0300

    first commit

---
+++ src/main/java/com/devops/asddevaula04/AsdDevAula04Application.java
@@ -0,0 +1,13 @@
+package com.devops.asddevaula04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsdDevAula04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsdDevAula04Application.class, args);
+	}
+
+}
